0234c4e9f3950ab869c4193f58d66bfd0c207209
Albert Arifullin <albert.arifullin@onetwotrip.com>
2023-07-24 15:14:39 +0300

    Added SPM dependency: Swinject

--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -3,7 +3,9 @@ import ProjectDescription
 let dependencies = Dependencies(
     carthage: [
         .github(path: "JonasGessner/JGProgressHUD", requirement: .upToNext("2.0.0")),
-    ],
+    ], swiftPackageManager: .init([
+        .remote(url: "https://github.com/Swinject/Swinject.git", requirement: .exact("2.8.3")),
+    ]),
     platforms: [.iOS]
 )
 
